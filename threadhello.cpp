cef8738fa2c8972b4c5be1f6e0ce08968627153e
Alexandre Bernardino <alex@isr.ist.utl.pt>
2015-12-09 04:07:40 -0800

    thread hello with yield

---
+++ threadhello.cpp
@@ -0,0 +1,24 @@
+//Compile with g++ -std=c++11 threadhello.cpp -pthread -o threadhello
+
+#include <thread>
+#include <iostream>
+using namespace std;
+void concurrent_function()  {
+  int counter = 0; 
+  while(++counter  < 10) {
+      cout << counter << ": Hello from Child Thread # "  <<   this_thread::get_id() << endl;
+      this_thread::yield(); //pass time slot to other thread - try to comment this line
+   }
+}
+int main()   {
+  cout << "Starting Main Thread #"  << this_thread::get_id() << endl;
+  thread t1 {concurrent_function};
+  cout << "Created Child Thread 1 # " << t1.get_id() << endl;
+  thread t2 {concurrent_function};
+  cout << "Created Child Thread 2 # "  << t2.get_id() << endl;
+  t1.join(); //wait until t1 finishes
+  t2.join(); //wait until t2 finishes
+  cout << "Finished." << endl;
+  getchar();
+}
+
